00fea15f9df0a006032f0b36a28d6115b6f14e06
CYH <whohasmu@gmail.com>
2024-12-05 20:55:07 +0900

    controller Interceptor 추가

--- src/main/java/com/cyh/base/config/WebConfig.java
+++ src/main/java/com/cyh/base/config/WebConfig.java
@@ -1,6 +1,6 @@
 package com.cyh.base.config;
 
-// import com.cyh.base.intercepter.ControllerInterceptor;
+import com.cyh.base.intercepter.ControllerInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -39,10 +39,10 @@ public class WebConfig implements WebMvcConfigurer {
             //             .allowCredentials(true); // 쿠키 전송 허용
     }
 
-//     @Override
-//     public void addInterceptors(InterceptorRegistry registry) {
-// //        registry.addInterceptor(new ControllerInterceptor()).excludePathPatterns("/static/**", "/favicon.ico");
-//     }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+       registry.addInterceptor(new ControllerInterceptor()).excludePathPatterns("/static/**", "/favicon.ico");
+    }
 
 
 }
--- src/main/java/com/cyh/base/intercepter/ControllerInterceptor.java
+++ src/main/java/com/cyh/base/intercepter/ControllerInterceptor.java
@@ -1,38 +1,38 @@
-// package com.cyh.base.intercepter;
+package com.cyh.base.intercepter;
 
 
-// import lombok.extern.slf4j.Slf4j;
-// import org.springframework.web.method.HandlerMethod;
-// import org.springframework.web.servlet.HandlerInterceptor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
 
 
-// import jakarta.servlet.http.HttpServletRequest;
-// import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 
 
-// @Slf4j
-// public class ControllerInterceptor implements HandlerInterceptor {
+@Slf4j
+public class ControllerInterceptor implements HandlerInterceptor {
 	
 
-// 	@Override
-// 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-// 		String path = request.getServletPath();
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		String path = request.getServletPath();
 
-// 		// logging
-// 		log.info("\n");
-// 		log.info("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ controller interceptor preHandle START");
-// 		log.info("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ path : {}", path);
-// //		log.info("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ request : {}", request.getParameterMap().toString());
-// //		log.info("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ response : {}", response.toString());
-// 		if (handler instanceof HandlerMethod) {
-// 			log.info("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ handler : {}", (HandlerMethod) handler);
-// 		}
-// 		log.info("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ controller interceptor preHandle END");
-// 		log.info("\n");
+		// logging
+		log.info("\n");
+		log.info("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ controller interceptor preHandle START");
+		log.info("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ path : {}", path);
+		log.info("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ request : {}", request.getParameterMap().toString());
+		log.info("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ response : {}", response.toString());
+		if (handler instanceof HandlerMethod) {
+			log.info("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ handler : {}", (HandlerMethod) handler);
+		}
+		log.info("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ controller interceptor preHandle END");
+		log.info("\n");
 
-// 		return true;
-// 	}
+		return true;
+	}
 
     
-// }
+}
